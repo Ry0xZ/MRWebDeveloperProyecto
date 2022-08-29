b59a9157a05a7fcbca5070dd333da8231d0c6f63
Matias Romero <romeromatias92@gmail.com>
2022-08-28 22:01:24 -0300

    sacando animaciones e implementando SEO

--- js/animacion.js
+++ js/animacion.js
@@ -1,18 +1,13 @@
-/* var nav = document.querySelector('nav');
-
-window.addEventListener('scroll', function() {
-    if(window.pageYOffset > 10) {
-        nav.classList.add('bg-dark', 'shadow');
-    } else {
-        nav.classList.remove('bg-dark', 'shadow');
-    }
-}); */
-
+/* barra de navegacion transparente y con fondo al hacer scroll */
 window.addEventListener("scroll", function() {
     var nav = document.querySelector("nav");
     nav.classList.toggle("bg-dark",window.scrollY>95);
 })
 
+/* Fin de  barra de navegacion transparente y con fondo al hacer scroll  */
+
+
+/* animacion de la pagina mis trabajos */
 document.addEventListener("click",function (e){
     if(e.target.classList.contains("gallery-item")){
         const src = e.target.getAttribute("src");
@@ -21,3 +16,5 @@ document.addEventListener("click",function (e){
         myModal.show();
     }
 })
+
+/* Fin de  animacion de la pagina mis trabajos */
